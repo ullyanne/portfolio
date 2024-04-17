cb3600159a8bee437982f13cae014b3e4f01c685
Ullyanne <ujfp@ic.ufal.br>
2024-04-17 15:37:51 -0300

    Atualiza configuração dos links da Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,12 +7,12 @@ export function Navbar() {
       <nav className="flex justify-end bg-slate-900 py-4 border-b border-slate-800/70 ">
         <ul className="flex flex-row gap-4 me-40 mt-2 text-slate-400 ">
           <li className="hover:text-slate-300 transition ease-in-out">
-            <a href="https://www.linkedin.com/in/ullyanne-patriota/">
+            <a target="_blank" href="https://www.linkedin.com/in/ullyanne-patriota/">
               <FontAwesomeIcon icon={faLinkedin} font-size="1.51em" />
             </a>
           </li>
           <li className="hover:text-slate-300 transition ease-in-out">
-            <a href="https://github.com/ullyanne/">
+            <a target="_blank" href="https://github.com/ullyanne/">
               <FontAwesomeIcon icon={faGithub} font-size="1.5em" />
             </a>
           </li>
