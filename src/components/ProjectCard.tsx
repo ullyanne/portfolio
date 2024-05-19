bf1b5bb88cc58d3994d39549df04edf7394ae30f
Ullyanne <ujfp@ic.ufal.br>
2024-05-19 20:21:13 -0300

    💄 fix project cards anchor nesting

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -12,7 +12,7 @@ export interface ProjectCardProps {
   site: string
 }
 
-export function ProjectCard({ id, title, description, technologies, site }: ProjectCardProps) {
+export function ProjectCard({ id, title, description, technologies }: ProjectCardProps) {
   const projectsComponents: { [key: number]: string } = {
     1: '/projects/inpaciente'
   };
@@ -22,12 +22,12 @@ export function ProjectCard({ id, title, description, technologies, site }: Proj
   };
 
   return (
-    <Link reloadDocument to={projectsComponents[id]} className="select-text rounded-md outline-none focus:ring-2 focus:ring-sky-300/80 focus-visible:ring-sky-300/80 text-left bg-[#162038] space-y-4 p-5 max-w-96 h-auto ">
+    <div className="select-text rounded-md outline-none focus:ring-2 focus:ring-sky-300/80 focus-visible:ring-sky-300/80 text-left bg-[#162038] space-y-4 p-5 max-w-96 h-auto ">
       <div className='flex justify-between'>
         <h3 className="text-xl font-semibold text-slate-200">{title}</h3>
-        <a target="_blank" className="hover:text-slate-300 transition ease-in-out focus-visible:ring-sky-300/80 focus:ring-2 outline-none" href={site} onClick={(e) => (e.stopPropagation())}>
+        <Link to={projectsComponents[id]} target="_blank" className="hover:text-slate-300 transition ease-in-out focus-visible:ring-sky-300/80 focus:ring-2 outline-none" >
           <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='lg' />
-        </a>
+        </Link>
       </div>
       <img className="rounded-md" src={projectsImages[id]} alt="" />
       <p className="text-base max-[320px]:h-36 h-36 md:h-32 lg:h-32 xl:h-32 text-justify">{description}</p>
@@ -38,6 +38,6 @@ export function ProjectCard({ id, title, description, technologies, site }: Proj
           })}
         </ul>
       </div>
-    </Link>
+    </div>
   )
 }
